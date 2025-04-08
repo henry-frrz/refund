ccd61344c7b5c70f3a93c4415b1f1dd4e2bd1f31
Henrique Ferraz <henrique07ferraz@gmail.com>
2025-04-08 08:03:31 -0300

    feat: create object for new expense

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const form = document.querySelector('form')
 const amount = document.querySelector('#amount')
+const expense = document.querySelector('#expense')
+const category = document.querySelector('#category')
 
 let amountValue = amount.value
 
@@ -8,6 +11,21 @@ amount.oninput = () => {
   amountValue = Number(amount.value.replace(hasCharactersRegex, '')) / 100
 
   amount.value = formatNumberBRL(amountValue, 'currency')
+}
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const newExpense = {
+    id: new Date().getTime(),
+    name: expense.value,
+    category: {
+      id: category.value,
+      name: category.options[category.selectedIndex].text,
+    },
+    amount: amountValue,
+    createdAt: new Date(),
+  }
 }
 
 const formatNumberBRL = (value, style) => {
